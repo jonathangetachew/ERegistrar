49108380766b31eefabe736d677813e7b91839c3
Jonathan Tuta <jonathangetachew@gmail.com>
2019-10-08 19:35:42 -0500

    First Commit: Created SpringBoot application. Added initial files.

---
+++ README.md
@@ -0,0 +1,12 @@
+# ERegistrar
+Exercise SpringBoot Application for my CS425 Software Engineering Course at Maharishi University of Management.
+
+### Checklist
+* [x] Spring Boot
+* [x] Maven
+* [x] Spring Devtools
+* [x] Thymeleaf
+* [x] Lombok
+* [ ] JPA / Hibernate
+* [ ] Spring Security
+
---
+++ src/main/java/edu/mum/se/eregistrar/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package edu.mum.se.eregistrar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Jonathan on 10/8/2019.
+ */
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = {"", "/", "/eregistrar", "/eregistrar/home"},
+					method = RequestMethod.GET)
+	public String index() {
+		return "home/index";
+	}
+
+}
